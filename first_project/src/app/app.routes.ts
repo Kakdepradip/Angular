e864bfb3bb8d873e75cd89db5461e5656ac2bf8e
Praddip kakde <kakdepradip21@gmail.com>
2025-12-16 23:57:25 +0530

    signal,reactive,attribute,databinding,admin component created

--- first_project/src/app/app.routes.ts
+++ first_project/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { Admin } from './Component/admin/admin';
+import { ControlFlow } from './Component/control-flow/control-flow';
+import { signal } from '@angular/core';
+import { Signal } from './Component/signal/signal';
+import { ReactiveForm } from './Component/reactive-form/reactive-form';
+import { Attribute } from './Component/attribute/attribute';
+import { DataBinding } from './Component/data-binding/data-binding';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'data-binding',
+   component:DataBinding
+   
+  },
+  {
+    path: 'admin',
+    component: Admin,
+  },
+  {
+    path: 'conrol-flow',
+    component: ControlFlow,
+  },
+  {
+    path: 'signl',
+    component: Signal,
+  },
+  {
+    path: 'reactiveform',
+    component: ReactiveForm,
+  },
+  {
+    path:'Attribute',
+    component:Attribute
+  }
+];
